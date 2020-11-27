0384df90c7e82623ec73d487329c68a62f78d57d
ednayssell7 <ednayrr@gmail.com>
2020-11-26 21:21:55 -0700

    get a specific element from serie

--- Pandas_example.py
+++ Pandas_example.py
@@ -1,5 +1,10 @@
+#first its necessary to install package pip install pandas and then import statement 
 import pandas as pd
 
 serie = pd.Series([4, 7, 6, 2, 1])
 
 print(serie)
+
+#get a specific element
+
+print(serie.loc[2])
